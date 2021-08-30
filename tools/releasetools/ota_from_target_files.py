5263e2d9645c1d695380e45d292dd389950a0446
ralf979 <ralf979@gmail.com>
2021-08-30 06:47:21 +0000

    add IronLOS art
    Change-Id: Ie174355a6a07b915bf5e364e2b15bc4433c19060

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -837,6 +837,21 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   if OPTIONS.backuptool:
     script.RunBackup("backup", sysmount, target_info.get('use_dynamic_partitions') == "true")
 
+  model = target_info.GetBuildProp("ro.product.model")
+  build = target_info.GetBuildProp("ro.build.date")
+
+  script.Print("- IronLOS Project ------------------------------");
+  script.Print(" _____                      _      _____  _____ ");
+  script.Print("|_   _|                    | |    |  _  |/  ___|");
+  script.Print("  | |   _ __   ___   _ __  | |    | | | |\ `--. ");
+  script.Print("  | |  | '__| / _ \ | '_ \ | |    | | | | `--. |");
+  script.Print(" _| |_ | |   | (_) || | | || |____\ \_/ //\__/ /");
+  script.Print(" \___/ |_|    \___/ |_| |_|\_____/ \___/ \____/ ");
+  script.Print("- By Ralf979 ------------------ Android 11.0.0 -");
+  script.Print(" Compiled: %s "%(build));
+  script.Print(" For: %s   "%(model));
+  script.Print("----------------------------------------------");
+
   # All other partitions as well as the data wipe use 10% of the progress, and
   # the update of the system partition takes the remaining progress.
   system_progress = 0.9 - (len(block_diff_dict) - 1) * 0.1
